d504962b5570795ec2a3442634cee46fa06cfbc1
stegen <James.Stegen@pnnl.gov>
2021-11-17 14:58:55 -0800

    Update OS_Sensor_Stats.r

--- OS_Sensor_Stats.r
+++ OS_Sensor_Stats.r
@@ -1,5 +1,6 @@
 # for doing stats on OS sensor data
 
-accur.dat = read.csv()
+accur.dat = read.csv(file = "Mean_Error.csv",stringsAsFactors = F)
+str(accur.dat)
 
-t.test(x = acccur.dat$Mean_Error[grep(pattern = "Atlas")])
+t.test(x = accur.dat$Mean_Error[grep(pattern = "Atlas",x = accur.dat$Sensor_Group)],y = accur.dat$Mean_Error[grep(pattern = "Other",x = accur.dat$Sensor_Group)])
